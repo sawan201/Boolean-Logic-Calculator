e5593a1b887215b76df51b8446bf9a2e9522a2b7
sawan201 <160764319+sawan201@users.noreply.github.com>
2024-04-29 12:14:37 -0500

    Create TokenType.h
    Added to make code management efficient

---
+++ Source Code/TokenType.h
@@ -0,0 +1,5 @@
+enum TokenType {                  // Define an enumeration to categorize different types of tokens.
+  TOKEN_VAR,                      // Enumerator for boolean variable.
+  TOKEN_OPERATOR,                 // Enumerator for operators.
+  TOKEN_PAREN                     // Enumerator for parentheses.
+};
